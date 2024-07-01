500bd6e5cc8d54578b9fe89fb01afac48f2ca50e
sofihuang <distance75432@email.com>
2024-07-01 23:32:39 +0200

    .

--- lib/presentation/introduction/widget/explore_button.dart
+++ lib/presentation/introduction/widget/explore_button.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:roamio/config/theme/app_theme.dart';
 import 'package:roamio/responsive/font_size_scaler.dart';
 
 class ExploreButton extends StatelessWidget {
@@ -9,18 +11,24 @@ class ExploreButton extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: ElevatedButton.icon(
-        onPressed: () {},
+        onPressed: () {
+          context.go('/home_screen');
+        },
         label: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Text(
             'Explore',
             style: TextStyle(
-              fontSize: FontSizeScaler.calculateFontSize(context, 16),
+              fontSize: FontSizeScaler.calculateFontSize(context, 12),
               fontWeight: FontWeight.w600,
+              color: AppTheme.appColor,
             ),
           ),
         ),
-        icon: const Icon(Icons.search_rounded),
+        icon: const Icon(
+          Icons.search_rounded,
+          size: 25,
+        ),
       ),
     );
   }
